5f2bd36f8826b18ea780e5be2a2acc007a0029f6
Jacob Schuster <jschuster8765@outlook.com>
2020-01-31 19:09:46 -0500

    Initial Commit

---
+++ mdt-tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mdt-tracker
+//
+//  Created by Jacob Schuster on 1/31/20.
+//  Copyright © 2020 Jacob Schuster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
